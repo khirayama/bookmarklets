ca77354f5bd905eb8c21cac76987a1288548e91e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-01-14 07:39:38 +0900

    Update to support video

--- audio-speed.js
+++ audio-speed.js
@@ -1,6 +1,6 @@
 javascript:(() => {
   let rate = Number(prompt('speed', '1.45'));
-  let els = document.querySelectorAll('audio');
+  let els = document.querySelectorAll('audio, video');
   for (let i = 0; i < els.length; i++) {
     els[i].playbackRate = rate;
   }
