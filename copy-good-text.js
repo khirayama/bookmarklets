1e3fb6e69c51524f93803c8cc49c1f43a97e948b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-01-23 08:47:43 +0900

    Create copy-good-text.js

---
+++ copy-good-text.js
@@ -0,0 +1,23 @@
+function copyTextToClipboard(text){
+  const body = document.querySelector('body');
+  const textarea = document.createElement('textarea');
+
+  textarea.textContent = text;
+
+  body.appendChild(textarea);
+  textarea.select();
+
+  const value = document.execCommand('copy');
+
+  body.removeChild(textarea);
+
+  return value;
+}
+
+function selectText(selector = 'body') {
+  const els = document.querySelector(selector).querySelectorAll('div,p,ul,li,span,a,dl,table');
+  const elList = [].slice.call(els);
+  const text = elList.map(el => el.innerText).join('');
+
+  copyTextToClipboard(text);
+}
