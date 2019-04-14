cfbaebdcf16f5e7dd6b21924c12a107c79bab493
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-04-14 14:46:48 +0900

    Create leetcode bookmarklet

---
+++ leetcode.js
@@ -0,0 +1,33 @@
+javascript: (() => {
+  const contents = [
+    // Id
+    document.querySelector('#question-title').innerText.split('.')[0].trim(),
+    // Name
+    document.querySelector('#question-title').innerText.split('.')[1].trim(),
+    // URL
+    location.href,
+    // Difficulty
+    document.querySelector('div[diff]').innerText.trim(),
+    // Topics
+    Array.from(document.querySelectorAll('a[href*="/tag/"]')).map((el) => el.innerText).join(' / '),
+    // Solution
+    document.querySelector('div[data-key=solution]').dataset.disabled !== 'true' ? 'Yes' : 'No',
+  ];
+
+  function execCopy(string) {
+    var tmp = document.createElement('div');
+    var pre = document.createElement('pre');
+    pre.style.webkitUserSelect = 'auto';
+    pre.style.userSelect = 'auto';
+    tmp.appendChild(pre).textContent = string;
+    var s = tmp.style;
+    s.position = 'fixed';
+    s.right = '200%';
+    document.body.appendChild(tmp);
+    document.getSelection().selectAllChildren(tmp);
+    var result = document.execCommand("copy");
+    document.body.removeChild(tmp);
+    return result;
+  }
+  execCopy(contents.join(','));
+})();
